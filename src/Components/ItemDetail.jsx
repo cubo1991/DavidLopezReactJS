aeb11e8c2e0e05c578697c0952188255b1677279
cubo1991 <adavidlopezmathez@gmail.com>
2022-03-27 19:14:42 -0300

    Boton Volver

--- src/Components/ItemDetail.jsx
+++ src/Components/ItemDetail.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import {Card} from 'react-bootstrap';
+import {Card, Button} from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const ItemDetail = ({ProductDetail}) => {
     const {title, price, pictureUrl} = ProductDetail
+    const Navigate = useNavigate()
+
+    const volver = () => {Navigate (-1)}
   return (
+    <div>
     <div className='d-flex justify-content-around'>
          <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src={pictureUrl} />
@@ -14,7 +19,9 @@ const ItemDetail = ({ProductDetail}) => {
     </Card.Text> 
       </Card.Body>
 </Card>
+
 </div>
+<Button className="btn  btn-ouline-primary" onClick={volver}> Volver</Button> </div>
   )
 }
 
